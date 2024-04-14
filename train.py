c96b889dadf3384d447856075ff0ceb0314f7008
Yucheng Wang <ycwang@YuchengdeMacBook-Pro.local>
2024-04-13 20:12:32 -0400

    revise model compression, using mixed precision training

--- train.py
+++ train.py
@@ -7,10 +7,14 @@ from model import IDPreservedGenerativeModel
 from torch.optim import Adam
 from tqdm import tqdm
 from torch.cuda.amp import autocast, GradScaler
+from transformers import ViTImageProcessor, ViTModel
+from PIL import Image
+import os
 
 DTYPE = torch.float16
+MAX_TRAINING_STEP = 501
 
-def train_model(model, data_loader, device, num_epochs=10, learning_rate=1e-4):
+def train_model(model, data_loader, dataset, device, num_epochs=1, learning_rate=5e-5):
     """
     Trains the IDPreservedGenerativeModel model.
 
@@ -23,67 +27,71 @@ def train_model(model, data_loader, device, num_epochs=10, learning_rate=1e-4):
     """
     model = model.to(device)
     optimizer = Adam(model.face_projection_layer.parameters(), lr=learning_rate)
-    # scaler = GradScaler()
-    
+    scaler = GradScaler()
+    global_step = 0
     print("Start Training...")
+    original_img_vit_cls_output = dataset.get_vit_cls_output()
     # Training loop
     for epoch in range(num_epochs):
         model.face_projection_layer.train()  # Set the model to training mode
         total_loss = 0
 
         for batch in tqdm(data_loader):
-            image_embedding = batch["vit_output"].to(device)
-            face_mask = batch["mask_face"].to(device)
-            hair_mask = batch["mask_hair"].to(device)
-            pixel_values = batch["face_img"].to(device)
-            prompt = batch['text']
-
-            # Forward pass
-            # with autocast():
-            model_pred, loss = model(
-                prompt=prompt,
-                pixel_values=pixel_values,
-                image_embedding=image_embedding,
-                face_mask=face_mask,
-                hair_mask=hair_mask,
-                device=device
-            )
+            if global_step>=MAX_TRAINING_STEP:
+                break
                 
-            # optimizer.zero_grad()
-            # scaler.scale(loss).backward()  # Scale the loss before the backward pass
-            # scaler.step(optimizer)  # Adjust the optimizer's step
-            # scaler.update()  # Update the scale for next iteration
-
-            # Backward pass and optimize
+            global_step += 1
+            image_embedding = batch["vit_output"].to(device, dtype = DTYPE)
+            face_mask = batch["mask_face"].to(device, dtype = DTYPE)
+            hair_mask = batch["mask_hair"].to(device, dtype = DTYPE)
+            pixel_values = batch["face_img"].to(device, dtype = DTYPE)
+            prompt = batch['text']
+    
             optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            # Forward pass with mixed precision
+            with autocast():
+                model_pred, loss = model(
+                    prompt=prompt,
+                    pixel_values=pixel_values,
+                    image_embedding=image_embedding,
+                    face_mask=face_mask,
+                    hair_mask=hair_mask,
+                    device=device
+                )
             
-            print(loss.item())
+            # print(loss.item())
+            # Backward pass and optimize
+            scaler.scale(loss).backward()  # Scale the loss before the backward pass
+            scaler.step(optimizer)  # Adjust the optimizer's step
+            scaler.update()  # Update the scale for next iteration
 
             total_loss += loss.item()
+            
+            save_path = os.path.join("./test_result",f"{dataset.get_img_name()}-{global_step}.pt")
+            if global_step%100==0:
+                model.save(original_img_vit_cls_output, save_path)
 
         avg_loss = total_loss / len(data_loader)
         print(f"Epoch [{epoch+1}/{num_epochs}], Average Loss: {avg_loss:.4f}")
 
-        # Optional: Save the model after each epoch
-        model.save_pretrained(f"model_epoch_{epoch+1}")
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"device: {device}")
 
-face_img_path = './donald_img.jpg'  # replace it with real path
+## test image
+face_img_path = '00001.png'  # replace it with real path 
+    
 print("Creating Dataset...")
 dataset = FaceDataset(face_img_path, device)
 print("Dataset Created!")
-data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
+data_loader = DataLoader(dataset, batch_size=1, shuffle=True)
 
 # Load and prepare your model
 print("Loading Pretrained Model...")
-model = IDPreservedGenerativeModel.from_pretrained("stabilityai/stable-diffusion-2-1", torch_dtype=DTYPE)
+model = IDPreservedGenerativeModel.from_pretrained("stabilityai/stable-diffusion-2-1",torch_dtype=torch.float32)
 model.scheduler = DPMSolverMultistepScheduler.from_config(model.scheduler.config)
 model.load_adaptor(device=device)
 
 # Call the training function
-train_model(model, data_loader, device, num_epochs=10, learning_rate=1e-4)
+train_model(model, data_loader, dataset, device, num_epochs=1, learning_rate=5e-5)
